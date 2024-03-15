ca8162c99839e6fbf974115eabedb35b3cf592a0
Anusha   Velagana <116190024+velagana@users.noreply.github.com>
2024-03-15 20:42:37 +0530

    Add files via upload

---
+++ 51.SIMPLE INTEREST.py
@@ -0,0 +1,8 @@
+#FIND THE SIMPLE INTEREST OF THE PROGRAMME
+# SIMPLE INTEREST = PTR/100
+p = int(input("enter the value of P:"))
+T = int(input("enter the value of T: "))
+R = int(input("enter the value of R: "))
+simple =p*T*R
+interest = simple/100
+print(interest)

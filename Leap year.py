fa00bfaca2fffd5b1363007c5b490c89d54e0772
anoo <19a81a0563@sves.org.in>
2024-03-16 15:59:53 +0530

    problem solved

--- Leap year.py
+++ Leap year.py
@@ -1,4 +1,5 @@
 #2. Create a program to determine if a given year is a leap year or not.
+#THIS IS THE CODE FOR LEAP YEAR PROGRAMME:
 year = int(input("Enter the year"))
 if (year % 4 == 0 and year %100 != 0) or (year % 400 == 0):
  print("The given year is Leap year")
